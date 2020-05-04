5e4eda624227466f5f9d4a57c19dd8f20492c145
inFamousxD <audimankar@gmail.com>
2020-05-05 01:20:20 +0530

    sidenav trigger color fix

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI_OFFLINE, {
+        const conn = await mongoose.connect(process.env.MONGO_URI_ONLINE, {
             useNewUrlParser: true,
             useCreateIndex: true,
             useFindAndModify: false,
