5f22aca631162a37aedd4301f98b41868a89c775
inFamousxD <audimankar@gmail.com>
2020-05-12 17:10:58 +0530

    remodelled router to take parameters

--- routes/storeval.js
+++ routes/storeval.js
@@ -1,11 +1,28 @@
 const express = require('express')
+const Input = require('../models/inputModel')
 const {
-    storeInput,
-    displayInput
+    storeAllInputs,
+    displayAllInputs,
+    displayInputCreation
 } = require('../controllers/storeval')
 
 const router = express.Router()
 
-router.route('/').get(displayInput).post(storeInput)
+router.route('/api/v1/storage').get(displayAllInputs).post(storeAllInputs)
+router.route('/api/v1/routetest').get(displayInputCreation)
+
+router.route('/api/v1/testparams/:idt')
+.get(function(req, res){
+    const identify = Input.findOne({identifier: req.params.idt}, function(err, data){
+        if(!err) {
+            return res.status(200).json ({
+                success: true,
+                data: data
+            })
+        } else {
+            console.log('Not found')
+        }
+    })
+})
 
 module.exports = router
